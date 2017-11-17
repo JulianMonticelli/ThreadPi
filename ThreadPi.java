4df3183945d8d1daf086e307a32cf0e3f921b025
JulianMonticelli <j.m.monticelli@gmail.com>
2017-11-16 22:13:51 -0500

    O(1) memory and 2^64-1 iterations max
    Signed-off-by: JulianMonticelli <j.m.monticelli@gmail.com>

--- ThreadPi.java
+++ ThreadPi.java
@@ -4,15 +4,15 @@ public class ThreadPi {
 
     public static void main(String[] args) {
 
-        int numIterations = 0;
+        long numIterations = 0;
         int numThreads = 0;
 
         if (args.length != 2) {
-            System.out.println("This program requires two integer arguments, numIterations and numThreads");
+            System.err.println("This program requires two integer arguments, numIterations and numThreads");
             System.exit(0);
         } else {
             try {
-                numIterations = Integer.parseInt(args[0]);
+                numIterations = Long.parseLong(args[0]);
                 numThreads = Integer.parseInt(args[1]);
             } catch (NumberFormatException nfe) {
                 nfe.printStackTrace();
@@ -22,31 +22,33 @@ public class ThreadPi {
         main.piFind(numIterations, numThreads);
     }
 
-    public void piFind(int numIterations, int numThreads) {
+    public void piFind(long numIterations, int numThreads) {
         if (numIterations < 10) {
-            System.out.println("You have too few iterations. The number of iterations should always be greater than or equal to 10.");
+            System.err.println("You have too few iterations. The number of iterations should always be greater than or equal to 10.");
             System.exit(0);
         }
 
         if (numThreads < 1) {
-            System.out.println("You must have at least one thread.");
+            System.err.println("You must have at least one thread.");
             System.exit(0);
         }
 
         if (numThreads > numIterations) {
-            System.out.println("Having more threads than iterations is nonsensical. Please fix this.");
+            System.err.println("Having more threads than iterations is nonsensical. Please fix this.");
             System.exit(0);
         }
+        
+        if (numIterations % numThreads != 0) {
+            System.err.println("It would be nice to have a number of iterations that is exactly divisible by the number of threads.");
+        }
 
-        float[] xPos = new float[numIterations];
-        float[] yPos = new float[numIterations];
 
         PiFinderThread[] thread = new PiFinderThread[numThreads];
 
-        int[] numIn = new int[numThreads];
+        long[] numIn = new long[numThreads];
 
         for (int i = 0; i < numThreads; i++) {
-            thread[i] = new PiFinderThread(i, numIterations / numThreads, xPos, yPos, numIn);
+            thread[i] = new PiFinderThread(i, numIterations / numThreads, numIn);
             thread[i].start();
         }
 
@@ -58,8 +60,8 @@ public class ThreadPi {
             }
         }
 
-        int totalIn = 0;
-        for (int i : numIn) {
+        long totalIn = 0;
+        for (long i : numIn) {
             totalIn += i;
         }
 
@@ -70,28 +72,26 @@ public class ThreadPi {
     }
     
     public class PiFinderThread extends Thread {
-        public PiFinderThread(int threadNum, int numberToDo, float[] xPos, float[] yPos, int[] numIn) {
+        public PiFinderThread(int threadNum, long numberToDo, long[] numIn) {
             this.threadNum = threadNum;
             this.numberToDo = numberToDo;
-            this.xPos = xPos;
-            this.yPos = yPos;
             this.numIn = numIn;   
         }
 
 
         int threadNum;
-        int numberToDo;
-        float[] xPos;
-        float[] yPos;
-        int[] numIn;
+        long numberToDo;
+        long[] numIn;
 
         @Override
         public void run() {
             Random rand = new Random();
-            for (int i = (threadNum * numberToDo); i < ( (threadNum + 1) * numberToDo); i++) {
-                xPos[i] = rand.nextFloat();
-                yPos[i] = rand.nextFloat();
-                if ( ( Math.pow(xPos[i], 2) + Math.pow(yPos[i], 2) ) < 1) {
+            float xPos;
+            float yPos;
+            for (long i = (threadNum * numberToDo); i < ( (threadNum + 1) * (numberToDo)); i++) {
+                xPos = rand.nextFloat();
+                yPos = rand.nextFloat();
+                if ( ( Math.pow(xPos, 2) + Math.pow(yPos, 2) ) < 1) {
                     numIn[threadNum]++;
                 }
             }
